46aedb72163a338e7cc370be376db5aa5253eadb
Entra21 <entra21@proway.treina>
2021-05-07 20:32:20 -0300

    Atualizando calculos

--- src/imc.java
+++ src/imc.java
@@ -5,17 +5,17 @@ public class imc {
 
     public static void main(String[] args) {
 
-
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Digite seu peso");
-        double peso = scanner.nextDouble();
+           System.out.println("Digite seu peso");
+           double peso = scanner.nextDouble();
 
-        System.out.println("Digite sua altura");
-        double altura = scanner.nextDouble();
+           System.out.println("Digite sua altura");
+           double altura = scanner.nextDouble();
 
-        double resultado = (peso) / (altura * altura);
+           double resultado = (peso) / (altura * altura);
 
-        System.out.println("O resuntado e " + resultado);
+           System.out.println("O resuntado e " + resultado);
+
     }
 }
