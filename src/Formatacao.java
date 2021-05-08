df18fb9f4b18d1e55f8ad91b2d4f7c9408e58b5a
Entra21 <entra21@proway.treina>
2021-05-07 21:11:46 -0300

    Metodos de formatacao

---
+++ src/Formatacao.java
@@ -0,0 +1,17 @@
+public class Formatacao {
+
+        public static void main(String[] args) {
+
+            double x = 10.35784;
+
+            // imprime o texto SEM uma nova linha (SEM QUEBRA DE TEXTO)
+            System.out.print(x);
+
+            // imprime o texto COM uma nova linha (COM QUEBRA DE TEXTO)
+            System.out.println(x);
+
+            // formata o resultado eliminando casa decimal
+            System.out.printf("%.2f%n", x);
+            System.out.printf("%.4f%n", x);
+        }
+}
