ede1d66cb76e61731dac97d208bd6f2cd0a3448d
Entra21 <entra21@proway.treina>
2021-05-11 21:13:39 -0300

    calculadora de area do trapezia

---
+++ src/CalculaAreaDoTrapezio.java
@@ -0,0 +1,20 @@
+public class CalculaAreaDoTrapezio {
+
+           public static void main(String[] args) {
+
+
+            double b, h, B, area;
+            float valor = 1.0F;
+            h = 5.0;
+            b = 6.2;
+            B = 8.0;
+
+            area = (b + B) / 2.0 * h;
+
+
+            System.out.println(area);
+
+
+        }
+
+}
