ff37141b59542197c79a87160f74e2561643a2eb
Entra21 <entra21@proway.treina>
2021-05-12 21:22:20 -0300

    funcao de bascara

---
+++ src/FuncaoDeBascara.java
@@ -0,0 +1,25 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class FuncaoDeBascara {
+
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+
+        Scanner scanner = new Scanner(System.in);
+
+        double delta, b, a, c, x1, x2;
+
+        double delta = Math.pow(b, 2.0) - 4 * a * c;
+
+        x1 = (-b + Math.sqrt(delta))/ (2.0 * a);
+        x2 = (-b - Math.sqrt(delta)) / (2.0 * a);
+
+        System.out.println("Resultado do x linha " + x1);
+        System.out.println("Resultado do x 2 linha  " + x2);
+
+        scanner.close();
+    }
+
+    }
+}
