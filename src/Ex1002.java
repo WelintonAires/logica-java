700725bc6c77c56d50524bd095c056e7f53d4a58
Welinton Oliveira <you@example.com>
2021-05-16 11:33:33 -0300

    Revert "atualizado pasta src"
    This reverts commit 0b2258b735f4d2ea3fcb94a59664ab869bd28e6b.

---
+++ src/Ex1002.java
@@ -0,0 +1,22 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Ex1002 {
+    public static void main(String[]args) {
+
+        Scanner scanner = new Scanner(System.in);
+        Locale.setDefault(Locale.US);
+
+
+        //DADOS DE ENTRADA
+        System.out.println("Digite o seu nome");
+        String n = scanner.nextLine();
+
+        //PROCESSAMENTO
+        double n = 3.14159
+
+         //SAIDA DE DADOS
+
+
+    }
+}
