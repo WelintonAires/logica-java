22ca66b09a55669fc94ff55f4b4f627b35136624
Welinton Oliveira <you@example.com>
2021-05-18 12:10:39 -0300

    Finalizado exercicio 1006

--- src/Ex1006.java
+++ src/Ex1006.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class Ex1006 {
+import java.util.Scanner;
+import java.util.Locale;
+
+public class Ex1006 {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner scanner = new Scanner(System.in);
+
+        double a = scanner.nextDouble();
+        double b = scanner.nextDouble();
+        double c = scanner.nextDouble();
+        double media = (a * 2 + b * 3 + c * 5) / 10.0;
+
+        System.out.printf("MEDIA = %.1f%n", media);
+
+        scanner.close();
+    }
 }
