b8ca85e02568a05b0a26556254b78d6680993c6b
Entra21 <entra21@proway.treina>
2021-05-25 19:49:16 -0300

    Operadores de comparacao

---
+++ src/OperadoresComparacao.java
@@ -0,0 +1,28 @@
+public class OperadoresComparacao {
+
+    public static void main(String[] args) {
+
+        //MENOR QUE
+        boolean menorQue = 5<10;
+        System.out.println("MENOR QUE => " + menorQue);
+
+        //MAIOR QUE
+        System.out.println(5 > 10);
+
+        //MENOR OU IGUAL A QUE
+        System.out.println(10 <= 10);
+
+        //MAIOR OU IGUAL A QUE
+        System.out.println(10 >= 10);
+
+        //IGUALDADE
+        System.out.println(5 == 10);
+
+        //DIFERENTE
+        System.out.println(5 != 10);
+
+
+    }
+
+
+}
