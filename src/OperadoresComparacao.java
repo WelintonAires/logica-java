df0f3ac3ff19b863b984f08e386a8a73f3afa53e
Entra21 <entra21@proway.treina>
2021-05-25 21:13:36 -0300

    Operadores Logicos

--- src/OperadoresComparacao.java
+++ src/OperadoresComparacao.java
@@ -22,7 +22,7 @@ public class OperadoresComparacao {
         System.out.println(5 != 10);
 
         System.out.println("-------------------------------------------------------------------------");
-        
+
 
         int x = 5;
         System.out.println(x > 0);    // true
@@ -30,6 +30,27 @@ public class OperadoresComparacao {
         System.out.println(10 <= 30); //true
         System.out.println(x != 2);   //true
 
+        System.out.println("---------------------OPERADORES LOGICOS --- AND ou E ----------------------");
+
+        System.out.println(x <= 20 && x == 10); //false
+
+        System.out.println(x > 0 && x != 3);    //true
+
+        System.out.println(x <= 20 && x == 10 && x != 3); //false
+
+        System.out.println("---------------------OPERADORES LOGICOS --- OR ou OU ----------------------");
+
+        System.out.println(x <= 20 || x == 10); //true
+
+        System.out.println(x > 0 || x != 3);    //true
+
+        System.out.println(x <= 20 || x == 10 || x != 3); //true
+
+        System.out.println("---------------------OPERADORES LOGICOS --- NÃO ou NOT ----------------------");
+
+        System.out.println(!(x == 10)); // NÃO É IGUAL A 10 -- TRUE
+        System.out.println(!(x >= 2));  // NÃO É MAIOR QUE 2 -- FALSE
+        System.out.println((x <= 20) && (x == 10)); // (V) (F) -- FALSE
 
     }
 
