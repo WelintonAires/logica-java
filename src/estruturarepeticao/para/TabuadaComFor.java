d2ca8ce80d0a58281410f0ea1ea0bde6340f808e
Your Name <mayconsouzamendes@gmail.com>
2021-06-16 20:10:56 -0300

    Tabuada com for

---
+++ src/estruturarepeticao/para/TabuadaComFor.java
@@ -0,0 +1,43 @@
+package estruturarepeticao.para;
+
+import java.util.Scanner;
+
+public class TabuadaComFor {
+    /**
+     * Leia 1 valor inteiro N (2 < N < 1000).
+     * A seguir, mostre a tabuada de N:
+     * 1 x N = N      2 x N = 2N        ...       10 x N = 10N
+     *
+     * Entrada
+     * A entrada contém um valor inteiro N (2 < N < 1000).
+     *
+     * Saída
+     * Imprima a tabuada de N, conforme o exemplo fornecido.
+     *
+     * Exemplo de Entrada	|    Exemplo de Saída
+     * 140                      1 x 140 = 140
+     *                          2 x 140 = 280
+     *                          3 x 140 = 420
+     *                          4 x 140 = 560
+     *                          5 x 140 = 700
+     *                          6 x 140 = 840
+     *                          7 x 140 = 980
+     *                          8 x 140 = 1120
+     *                          9 x 140 = 1260
+     *                          10 x 140 = 1400
+     */
+    public class ExUri1078 {
+
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+            int n = scanner.nextInt();
+
+            for (int i = 1; i <= 3; i++){
+                int produto = i * n;
+                System.out.println(i + " X " + n + " = " + produto );
+            }
+            scanner.close();
+        }
+    }
+
+}
